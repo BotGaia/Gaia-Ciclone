308c92678ef66c00479a8bdfbae45306a8b14eb0
Sam Buters <sambuters@gmail.com>
2019-06-13 11:58:28 -0300

    Fixing #247 Fixing bugs

--- src/requests/cycloneRequest.js
+++ src/requests/cycloneRequest.js
@@ -19,36 +19,21 @@ module.exports = {
           if (response.data.success) {
             if (response.data.error) {
               resolve(response);
-            } else if (response.data.response.isArray) {
-              await readCyclones.deleteAllCyclones();
-              response.data.response.forEach((cycloneElement) => {
-                const cyclone = new Cyclone(
-                  cycloneElement.profile.name,
-                  treatCycloneDetails.treatBasin(cycloneElement.profile.basinOrigin),
-                  treatCycloneDetails.treatBasin(cycloneElement.profile.basinCurrent),
-                  cycloneElement.profile.lifespan.startDateTimeISO,
-                  cycloneElement.profile.lifespan.endDateTimeISO,
-                  treatCycloneDetails.treatStormType(cycloneElement.position.details.stormType),
-                  cycloneElement.position.details.windSpeedKPH / 3.6,
-                );
-                cyclone.saveCyclone();
-              });
-              resolve(response.data);
-            } else {
-              await readCyclones.deleteAllCyclones();
+            }
+            await readCyclones.deleteAllCyclones();
+            response.data.response.forEach((cycloneElement) => {
               const cyclone = new Cyclone(
-                response.data.response.profile.name,
-                treatCycloneDetails.treatBasin(response.data.response.profile.basinOrigin),
-                treatCycloneDetails.treatBasin(response.data.response.profile.basinCurrent),
-                response.data.response.profile.lifespan.startDateTimeISO,
-                response.data.response.profile.lifespan.endDateTimeISO,
-                treatCycloneDetails.treatStormType(
-                  response.data.response.position.details.stormType,
-                ),
-                response.data.response.position.details.windSpeedKPH / 3.6,
+                cycloneElement.profile.name,
+                treatCycloneDetails.treatBasin(cycloneElement.profile.basinOrigin),
+                treatCycloneDetails.treatBasin(cycloneElement.profile.basinCurrent),
+                cycloneElement.profile.lifespan.startDateTimeISO,
+                cycloneElement.profile.lifespan.endDateTimeISO,
+                treatCycloneDetails.treatStormType(cycloneElement.position.details.stormType),
+                cycloneElement.position.details.windSpeedKPH / 3.6,
               );
               cyclone.saveCyclone();
-            }
+            });
+            resolve(response.data);
           } else {
             resolve(response.data);
           }
