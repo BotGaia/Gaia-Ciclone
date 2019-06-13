15e7d40da92fc7c8d7f6d7963467f479b0e53c34
Sam Buters <sambuters@gmail.com>
2019-06-13 20:25:13 -0300

    Hotfixing #247 Removing unnecessary else and moving non-conditional statements

--- src/requests/cycloneRequest.js
+++ src/requests/cycloneRequest.js
@@ -17,26 +17,23 @@ module.exports = {
       axios.get('https://api.aerisapi.com/tropicalcyclones', { params })
         .then(async (response) => {
           if (response.data.success) {
-            if (response.data.error) {
-              resolve(response);
-            }
             await readCyclones.deleteAllCyclones();
-            response.data.response.forEach((cycloneElement) => {
-              const cyclone = new Cyclone(
-                cycloneElement.profile.name,
-                treatCycloneDetails.treatBasin(cycloneElement.profile.basinOrigin),
-                treatCycloneDetails.treatBasin(cycloneElement.profile.basinCurrent),
-                cycloneElement.profile.lifespan.startDateTimeISO,
-                cycloneElement.profile.lifespan.endDateTimeISO,
-                treatCycloneDetails.treatStormType(cycloneElement.position.details.stormType),
-                cycloneElement.position.details.windSpeedKPH / 3.6,
-              );
-              cyclone.saveCyclone();
-            });
-            resolve(response.data);
-          } else {
-            resolve(response.data);
+            if (Array.isArray(response.data.response)) {
+              response.data.response.forEach((cycloneElement) => {
+                const cyclone = new Cyclone(
+                  cycloneElement.profile.name,
+                  treatCycloneDetails.treatBasin(cycloneElement.profile.basinOrigin),
+                  treatCycloneDetails.treatBasin(cycloneElement.profile.basinCurrent),
+                  cycloneElement.profile.lifespan.startDateTimeISO,
+                  cycloneElement.profile.lifespan.endDateTimeISO,
+                  treatCycloneDetails.treatStormType(cycloneElement.position.details.stormType),
+                  cycloneElement.position.details.windSpeedKPH / 3.6,
+                );
+                cyclone.saveCyclone();
+              });
+            }
           }
+          resolve(response.data);
         }).catch((err) => {
           resolve(err.response);
         });
