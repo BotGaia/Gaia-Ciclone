0946d7ba7030742019d09605ddc4e28314f66e63
Sam Buters <sambuters@gmail.com>
2019-06-23 22:44:18 -0300

    Adding cyclone deletion test

--- src/test/cycloneTest.js
+++ src/test/cycloneTest.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-undef */
 const chai = require('chai');
 const Cyclone = require('../models/CycloneModel');
-const ReadCyclones = require('../utils/readCyclonesUtil');
+const cycloneReading = require('../utils/readCyclonesUtil');
 const cycloneRequest = require('../requests/cycloneRequest');
 const Timer = require('../utils/cycloneTimerUtil');
 
@@ -19,19 +19,36 @@ const mockCyclone = new Cyclone(
   3.6,
 );
 
-describe('Test Cyclone', () => {
-  it('Should read test cyclone', () => {
+describe('Cyclone Reading', () => {
+  it('should read test cyclone', () => {
     mockCyclone.saveCyclone();
-    ReadCyclones.readCyclones().then((cyclones) => {
+    cycloneReading.readCyclones().then((cyclones) => {
+      cyclones.should.be.a('Array').that.has.lengthOf.at.least(1);
+
       cyclones.forEach((element) => {
         if (element.name === 'testName') {
           element.currentBasin.should.equal('AL');
         }
       });
     });
+
     mockCyclone.deleteMe();
   });
 
+  it('should delete all cyclones', (done) => {
+    cycloneReading.deleteAllCyclones().then(() =>  {
+      cycloneReading.readCyclones().then((cyclones) => {
+        cyclones.should.be.a('Array').that.has.lengthOf(0);
+      });
+    });
+  });
+});
+
+describe('Cyclone Timer', () => {
+  it('should run the timer');
+});
+
+describe('Uncategorized Cyclone Tests', () => {
   it('Should make a cyclone resquest', () => {
     cycloneRequest.getAllCyclones().then((cyclone) => {
       cyclone.success.should.equal(true);
